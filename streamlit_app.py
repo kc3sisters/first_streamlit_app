8a55abfb9071cfce3d1401e64c9604096adf80d5
kc3sisters <139355348+kc3sisters@users.noreply.github.com>
2023-07-24 22:59:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,14 @@ if streamlit.button("Get Fruit Load List"):
    streamlit.dataframe(my_data_rows)
 
 
+#Allow the ned use to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+         return "thanks for adding " + new_fruit
+
+
+
 streamlit.stop()
 #the ff codes are the old one without using function
 #import snowflake.connector
@@ -83,6 +91,8 @@ my_data_row = my_cur.fetchall()
 #streamlit.text("The fruit load list contains:")
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+
 
 
 fruit_choice1 = streamlit.text_input('What fruit would you like to add?')   
