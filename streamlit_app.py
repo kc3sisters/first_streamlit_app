a74f4f1dd9131a8551523cac7b382f72314437e8
kc3sisters <139355348+kc3sisters@users.noreply.github.com>
2023-07-24 21:25:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,25 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) - json (removed)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
+#New Section t odisplay fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+     strealit.error("Please select a fruit to get information.")
+  else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json()) - json (removed)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLerror as e:
+    streamlit.error()
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
@@ -51,7 +59,8 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-fruit_choice1 = streamlit.text_input('What fruit would you like to add?')
+
+fruit_choice1 = streamlit.text_input('What fruit would you like to add?')   
 streamlit.write('Thanks for adding ', fruit_choice1)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
